e87f8b9cd78d03a8aeb295739e674ef9221b6789
CandyFiend <16528385+CandyFiend@users.noreply.github.com>
2026-02-04 19:51:10 +0300

    Add two nodedef fields to prevent wield animation

--- src/itemdef.h
+++ src/itemdef.h
@@ -100,6 +100,9 @@ struct ItemDefinition
 	video::SColor color; // The fallback color of the node.
 	v3f wield_scale;
 
+	bool skip_wield_anim_on_stack; // Skip the wield animation if this item is equipped
+	bool skip_wield_anim_on_meta; // Skip the wield anim if the item's meta has changed
+
 	/*
 		Item stack and interaction properties
 	*/
